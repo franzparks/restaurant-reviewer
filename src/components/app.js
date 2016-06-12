57c9bd315f63991b9e5a2280b7194da890416240
franzparks <franzparks@gmail.com>
2016-06-11 17:26:27 -0700

    add header to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Restaurant Reviewer Coming Soon!</div>
+      <div>
+      	<Header />
+      </div>
     );
   }
 }
