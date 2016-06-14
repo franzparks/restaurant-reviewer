4d1075ac747be06c93fb28976f4b98996aba8347
franzparks <franzparks@gmail.com>
2016-06-14 07:37:38 -0700

    add list elements to hold restaurants

--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -4,8 +4,11 @@ import {connect} from 'react-redux';
 export default class RestaurantContainer extends Component {
 	render(){
         return (
-        <div>
-        	Am a container!
+        <div className="nav">
+        	<ul>
+        	   <li>First Restaurant </li>
+        	   <li> Second Restaurant </li>  
+        	</ul>
         </div>
         );
 	}
