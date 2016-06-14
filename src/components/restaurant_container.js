d630c8d104a80ecabd7461f1dde43d40d23e7a34
franzparks <franzparks@gmail.com>
2016-06-13 22:12:40 -0700

    initialize container

---
+++ src/components/restaurant_container.js
@@ -0,0 +1,12 @@
+var React, {Component} = require('react');
+var {connect} = require('react-redux');
+
+export default RestaurantContainer extends Component {
+	render : function(){
+        return (
+        <div>
+        	Am a container!
+        </div>
+        );
+	}
+}
