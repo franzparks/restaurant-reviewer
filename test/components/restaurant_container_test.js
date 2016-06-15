006ef1a8927c310e1c84ab3c505a80cfb3ca73c6
franzparks <franzparks@gmail.com>
2016-06-14 20:45:23 -0700

    add container test case

---
+++ test/components/restaurant_container_test.js
@@ -0,0 +1,14 @@
+import { renderComponent , expect } from '../test_helper';
+import RestaurantContainer from '../../src/components/restaurant_container';
+
+describe('header' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(RestaurantContainer);
+  });
+
+  it('renders the app header', () => {
+    expect(component).to.exist;
+  });
+});
