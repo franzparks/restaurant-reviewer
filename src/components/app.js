ac80da47675cde026a252c0b00a6738b18bd6600
franzparks <franzparks@gmail.com>
2016-06-15 20:11:23 -0700

    add initial template for footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './header';
+import Footer from './footer';
 import RestaurantContainer from './restaurant_container';
 
 export default class App extends Component {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div className="container">
       	<Header />
       	<RestaurantContainer />
+      	<Footer />
       </div>
     );
   }
