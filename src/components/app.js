0fb631eea5888d83e076e35c9af893f3c6f9d15e
franzparks <franzparks@gmail.com>
2016-06-16 14:01:57 -0700

    add main layout component to the app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './header';
 import Footer from './footer';
+import MainLayout from './main_layout';
 import RestaurantContainer from './restaurant_container';
 
 export default class App extends Component {
@@ -10,9 +11,11 @@ export default class App extends Component {
     	<Header />
         <div className="container">
             <div className="row row-offcanvas row-offcanvas-right">
+                <MainLayout />
       	        <RestaurantContainer />
       	        <Footer />
             </div>
+
         </div> 
       </div>     
     );
