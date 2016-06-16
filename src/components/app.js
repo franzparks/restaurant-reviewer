ded1ec525f4b4f0a9c378d238e5f6d0cd141827d
franzparks <franzparks@gmail.com>
2016-06-15 20:20:54 -0700

    move header to top of page

--- src/components/app.js
+++ src/components/app.js
@@ -6,13 +6,15 @@ import RestaurantContainer from './restaurant_container';
 export default class App extends Component {
   render() {
     return (
+      <div>
     	<Header />
         <div className="container">
             <div class="row row-offcanvas row-offcanvas-right">
       	        <RestaurantContainer />
       	        <Footer />
             </div>
-        </div>    
+        </div> 
+      </div>     
     );
   }
 }
