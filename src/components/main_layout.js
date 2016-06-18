b7ea3af0f4a57fa66617ee4332321772bd93a83c
franzparks <franzparks@gmail.com>
2016-06-17 19:58:01 -0700

    move restaurants from main layout to restaurant container

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import RestaurantContainer from './restaurant_container';
 import Overview from './overview';
 import Footer from './footer';
+import ToggleNav from './toggle_nav';
 
 class MainLayout extends Component {
 
@@ -10,41 +11,13 @@ class MainLayout extends Component {
 	  	    <div className="container">
 	            <div className="row row-offcanvas row-offcanvas-right">
 	                 <div className="col-xs-12 col-sm-9">
-				          <p className="pull-right visible-xs">
-				            <button type="button" className="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
-				          </p>
+				           <ToggleNav />
 				           <Overview />
-				          <div className="row">
-				            <div className="col-xs-6 col-lg-4">
-				              <h2>Heading</h2>
-				              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-				              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				            </div>
-				            <div className="col-xs-6 col-lg-4">
-				              <h2>Heading</h2>
-				              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-				              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				            </div>
-				            <div className="col-xs-6 col-lg-4">
-				              <h2>Heading</h2>
-				              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-				              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				            </div>
-				            <div className="col-xs-6 col-lg-4">
-				              <h2>Heading</h2>
-				              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-				              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				            </div>
-				          </div>
-				        </div>
+				           <RestaurantContainer />
 
-				        <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
-				          <div className="list-group">
-				            <a href="#" className="list-group-item active">By Location</a>
-				            <a href="#" className="list-group-item">By Cuisine</a>
-				            <a href="#" className="list-group-item">By Ratings</a>           
-				          </div>
-				        </div>
+				      </div>
+
+				        <Menu />
 	      	        
 	            </div>
 
--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -3,14 +3,29 @@ import {connect} from 'react-redux';
 
 export default class RestaurantContainer extends Component {
 	render(){
-        return (
-        <div className="nav">
-        	<ul className="list-group">
-        	   <li className="list-group-item">First Restaurant </li>
-        	   <li className="list-group-item"> Second Restaurant </li>
-        	   <li className="list-group-item"> Third Restaurant </li>  
-        	</ul>
-        </div>
+            return (
+            <div className="row">
+                                                    <div className="col-xs-6 col-lg-4">
+                                                      <h2>Heading</h2>
+                                                      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                                                      <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                                    </div>
+                                                    <div className="col-xs-6 col-lg-4">
+                                                      <h2>Heading</h2>
+                                                      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                                                      <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                                    </div>
+                                                    <div className="col-xs-6 col-lg-4">
+                                                      <h2>Heading</h2>
+                                                      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                                                      <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                                    </div>
+                                                    <div className="col-xs-6 col-lg-4">
+                                                      <h2>Heading</h2>
+                                                      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                                                      <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                                    </div>
+                                                  </div>
         );
 	}
 }
