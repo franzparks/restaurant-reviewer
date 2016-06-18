26d09dbc64c2c830e1f69cc7612265c02f5bb608
franzparks <franzparks@gmail.com>
2016-06-17 21:19:38 -0700

    add restuarant component to restaurant container

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Restaurant = () => {
-
+    <div className="col-xs-6 col-lg-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce 
+                         dapibus, tellus ac cursus commodo, tortor mauris condime
+                         uada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div>
 }
 export default Restaurant;
--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Restaurant from './restaurant';
 //import {connect} from 'react-redux';
 
 const RestaurantContainer = () => {
   
         return (
                 <div className="row">
+                    <Restaurant />
                     <div className="col-xs-6 col-lg-4">
                       <h2>Heading</h2>
                       <p>Donec id elit non mi porta gravida at eget metus. Fusce 
