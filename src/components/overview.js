7311de231b04c95e4e0b3cf936065589e2dfc437
franzparks <franzparks@gmail.com>
2016-06-17 19:24:59 -0700

    add overview component

---
+++ src/components/overview.js
@@ -0,0 +1,10 @@
+const Overview = () => {
+    return (
+        <div className="jumbotron">
+		    <h1>Restaurant Reviewer</h1>
+			<p>An Application To Rate And Review Restaurants.</p>
+	    </div>
+    );
+}
+
+export default Overview;
