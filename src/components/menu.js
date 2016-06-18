e5bf91ea3a91a624a8b2e745b0aa368dc547fbc1
franzparks <franzparks@gmail.com>
2016-06-17 19:49:22 -0700

    add menu component to main layout

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Menu = () => {
-  
+    <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+		<div className="list-group">
+		    <a href="#" className="list-group-item active">By Location</a>
+		    <a href="#" className="list-group-item">By Cuisine</a>
+		    <a href="#" className="list-group-item">By Ratings</a>           
+		</div>
+	</div>
 }
 
 export default Menu;
