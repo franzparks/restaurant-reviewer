78aa622b67641ff7813d44bb872c8fa8973e3f88
franzparks <franzparks@gmail.com>
2016-06-19 12:02:17 -0700

    add form

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const SearchBar = () => {
-
+    return (
+        <form>
+        	<input
+        	    type="text"
+        	    placeholder="Name, Location"
+        	    value={this.props.filterText}
+        	/>    
+        </form>
+    );
 }
 
 export default SearchBar;
