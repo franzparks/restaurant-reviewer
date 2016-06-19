f6ffdd37fbd4401563a3095e857019ae0c372c3c
franzparks <franzparks@gmail.com>
2016-06-18 21:20:37 -0700

    clean up

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -11,15 +11,11 @@ import ToggleNav from './toggle_nav';
 class MainLayout extends Component {
 
 	componentWillMount() {
-		console.log("here");
-		console.log("props : "+this.props);
 		this.props.fetchRestaurants();
 	}
 
 	render() {
-		//let RESTAURANTS = [{"name":"Lusaka Restaurant", "address":"101 Lsk", "image":"image","cuisineType":"Zambian"},
-		//{"name":"Ndola", "address":"101 Ndola", "image":"image","cuisineType":"Zambian"},
-		//{"name":"Livingstone", "address":"101 Lvstone", "image":"image","cuisineType":"Zambian"}];
+		
 	    return (
 	  	    <div className="container">
 	            <div className="row row-offcanvas row-offcanvas-right">
@@ -38,7 +34,6 @@ class MainLayout extends Component {
 	}  	
 }
 function mapStateToProps(state) {
-	console.log("state : "+state.restaurants);
 	return { restaurants : state.restaurants };
 }
 export default connect(mapStateToProps,actions)(MainLayout);
