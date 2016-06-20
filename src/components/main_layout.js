079473ebe54fcd27b56ab7907a8c768c8939edfe
franzparks <franzparks@gmail.com>
2016-06-20 16:37:21 -0700

    move header to main layout

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -7,6 +7,7 @@ import Overview from './overview';
 import Footer from './footer';
 import Menu from './menu';
 import ToggleNav from './toggle_nav';
+import Header from './header';
 
 class MainLayout extends Component {
 
@@ -17,20 +18,23 @@ class MainLayout extends Component {
 	render() {
 		
 	    return (
-	  	    <div className="container">
-	            <div className="row row-offcanvas row-offcanvas-right">
-	                <div className="col-xs-12 col-sm-9">
-				        <ToggleNav />
-				        <Overview />
-				        <RestaurantContainer 
-				            restaurants={this.props.restaurants}
-				            filterCategory={this.props.filterCategory}/>
-				            value={this.props.filterText}
-				    </div>
-				    <Menu />	      	        
-	            </div>
-	            <Footer />
-	        </div> 
+            <div>
+                <Header />
+		  	    <div className="container">
+		            <div className="row row-offcanvas row-offcanvas-right">
+		                <div className="col-xs-12 col-sm-9">
+					        <ToggleNav />
+					        <Overview />
+					        <RestaurantContainer 
+					            restaurants={this.props.restaurants}
+					            filterCategory={this.props.filterCategory}/>
+
+					    </div>
+					    <Menu />	      	        
+		            </div>
+		            <Footer />
+		        </div> 
+	        </div>
 	  	);
 	}  	
 }
