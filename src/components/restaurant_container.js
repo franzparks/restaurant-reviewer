7abc0a2777604be01b894c81d7fae8e59ab4e575
franzparks <franzparks@gmail.com>
2016-06-20 21:15:27 -0700

    add filter conditions

--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -4,15 +4,22 @@ import Restaurant from './restaurant';
 const RestaurantContainer = (props) => {
     
         var results = [];
+        var filter = props.filterText;
 
         props.restaurants.forEach((restaurant) => {
-             results.push(<Restaurant key={restaurant.name}
-                name={restaurant.name}
-                image={restaurant.image}
-                address={restaurant.address}
-                cuisineType={restaurant.cuisineType}
-                /> 
-             );
+            
+            if((restaurant.name).indexOf(filter) !== -1 || 
+                (restaurant.address).indexOf(filter) !== -1 ||
+                (restaurant.cuisineType).indexOf(filter) !== -1)
+
+                results.push(<Restaurant key={restaurant.name}
+                    name={restaurant.name}
+                    image={restaurant.image}
+                    address={restaurant.address}
+                    cuisineType={restaurant.cuisineType}
+                    /> 
+
+            );
         });
 
         return (
