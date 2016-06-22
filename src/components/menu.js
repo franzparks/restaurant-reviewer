a6ca04103dd7a090fca59015aa5f64e8cf49c108
franzparks <franzparks@gmail.com>
2016-06-21 19:42:14 -0700

    handle all selection

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Menu = () => {
+const Menu = (props) => {
 	return (
 	    <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
 			<div className="list-group">
-			    <a href="#" className="list-group-item active">All</a>
+			    <a href="#" className="list-group-item active" onClick={()=> props.handleSelection('all')}>All</a>
 			    <a href="#" className="list-group-item">Highest Rated</a>
 			    <a href="#" className="list-group-item">Most Reviewed</a>
 			    <a href="#" className="list-group-item">Open Now</a>           
