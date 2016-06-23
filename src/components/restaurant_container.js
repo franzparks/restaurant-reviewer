a696e6947cd5098e19cdb15fb9471e8d03791baa
franzparks <franzparks@gmail.com>
2016-06-22 21:12:25 -0700

    implement getNumberOfRatings

--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -5,13 +5,17 @@ const RestaurantContainer = (props) => {
     
         var results = [];
         var filter = props.filterText;
-        
+
         var getAverageRating = function(restaurant){
             if(restaurant.reviews.length > 1){
                 var sum = restaurant.reviews.reduce( (prev,curr) => prev + curr );
                 return sum / restaurant.reviews.length;
             }else
                 return 0; 
+        }
+
+        var getNumberOfRatings = (restaurant) => {
+            return restaurant.reviews.length;
         }
 
         props.restaurants.forEach((restaurant) => {
