58452097713a24c945116151881242c0f2ab5761
franzparks <franzparks@gmail.com>
2016-06-23 17:24:12 -0700

    clean up

--- src/components/restaurant_container.js
+++ src/components/restaurant_container.js
@@ -7,57 +7,23 @@ class RestaurantContainer  extends Component {
         constructor(props) {
         super(props);
         this.state = {
-            results : [], 
+            //results : [], 
             filter : props.filterText,
             filterCategory: props.filterCategory,
-            restaurants : props.restaurants
+            //restaurants : props.restaurants
         };
-        this.getAverageRating = this.getAverageRating.bind(this);
-        this.getNumberOfRatings = this.getNumberOfRatings.bind(this);
+        
         this.sortByHighestRated = this.sortByHighestRated.bind(this);
         this.sortByMostReviewed = this.sortByMostReviewed.bind(this);
         this.sortResults       = this.sortResults.bind(this);
-        this.loadData          = this.loadData.bind(this);
 
         }
 
         componentDidMount() {
-        console.log("am here :"+this.state.restaurants);
-          this.loadData([{"name":"hello","reviews":[]}]);
+        //console.log("am here :"+this.state.restaurants);
+        //  this.loadData([{"name":"hello","reviews":[]}]);
         }
 
-        loadData = (data) => {
-           console.log("I got called : "+data);
-            data.forEach((restaurant) => {
-                //console.log(" restaurant : "+restaurant);     
-                this.state.results.push(
-                    <Restaurant 
-                    key={restaurant.name + Math.random()*10000}
-                    name={restaurant.name}
-                    image={restaurant.image}
-                    address={restaurant.address}
-                    cuisineType={restaurant.cuisineType}
-                    averageRating = {this.getAverageRating(restaurant)}
-                    numberOfRatings={this.getNumberOfRatings(restaurant)}
-
-                    /> 
-
-                );
-
-            });
-        }
-
-        getAverageRating = (restaurant) => {
-            if(restaurant.reviews.length > 1){
-                var sum = restaurant.reviews.reduce( (prev,curr) => prev + curr );
-                return sum / restaurant.reviews.length;
-            }else
-                return 0; 
-        }
-
-        getNumberOfRatings = (restaurant) => {
-            return restaurant.reviews.length;
-        }
 
         sortByHighestRated = (results,restaurant) => {
             return this.sortResults(results, restaurant.props.averageRating);
