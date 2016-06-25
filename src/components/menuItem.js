f055b6e3da66ab22eadb3cbb193e804a0d6944ae
franzparks <franzparks@gmail.com>
2016-06-25 15:54:38 -0700

    implement menu item

--- src/components/menuItem.js
+++ src/components/menuItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const MenuItem = (props) =>{
-
+	<a 
+		href="#"
+		name={props.name} 
+	    className={props.style}
+	 	onClick={props.handleClick} >
+	 	{props.label}
+	 	</a>
 }
 
 export default MenuItem;
