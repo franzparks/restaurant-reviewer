f5f5ffd3f288ed50a2cbd4b31e20f9a5adbfc9a5
franzparks <franzparks@gmail.com>
2016-06-26 21:05:19 -0700

    import react router components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+
 
 import App from './components/app';
 import reducers from './reducers';
