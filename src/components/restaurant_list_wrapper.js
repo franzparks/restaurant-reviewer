35d483eabeae3d48bee898ed24696798f4b2c39a
franzparks <franzparks@gmail.com>
2016-06-28 21:15:28 -0700

    implement list wrapper

--- src/components/restaurant_list_wrapper.js
+++ src/components/restaurant_list_wrapper.js
@@ -1,2 +1,8 @@
  import React from 'react'
  import RestaurantList from './restaurant_list_wrapper';
+
+ const RestaurantListWrapper = (props) => {
+ 	return (
+ 	);
+ }
+ export default RestaurantListWrapper;
