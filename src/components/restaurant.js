633237be7a122ffe1c72bd60186d36564a3a8b93
franzparks <franzparks@gmail.com>
2016-06-29 20:53:11 -0700

    pass path param

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -10,7 +10,7 @@ const Restaurant = (props) => {
 	        <p>Address: {props.address}</p>
 	        <p>Type: {props.cuisineType}</p>
 	        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-	        <Link to="details" restaurant={props.rawData}> Details</Link>
+	        <Link to="restaurants/"+{props.name} restaurant={props.rawData}> Details</Link>
 
 	    </div>
     );
