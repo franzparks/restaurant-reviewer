30b09cb0765e74ee0be1172a4fe33d514eaf7346
franzparks <franzparks@gmail.com>
2016-06-29 21:12:25 -0700

    fix path

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 const Restaurant = (props) => {
 	console.log("raw data "+Object.keys(props.rawData));
+	var path = `restaurants/${props.name}`;
 	return(
 	    <div className="col-xs-6 col-lg-4">
 	        <h2>{props.name}</h2>
@@ -10,7 +11,7 @@ const Restaurant = (props) => {
 	        <p>Address: {props.address}</p>
 	        <p>Type: {props.cuisineType}</p>
 	        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-	        <Link to="restaurants/"+{props.name} restaurant={props.rawData}> Details</Link>
+	        <Link to={path} > Details</Link>
 
 	    </div>
     );
