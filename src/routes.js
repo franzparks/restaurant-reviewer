41e4e87c1a1255dea43714526417a6533b19d773
franzparks <franzparks@gmail.com>
2016-06-30 13:50:39 -0700

    add routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import MainLayout from './components/main_layout';
+import RestaurantListWrapper from './components/restaurant_list_wrapper';
+import RestaurantDetails from './components/restaurant_details';
+
+
+export default (
+  <Route path="/" component={MainLayout}>
+    <IndexRoute component={RestaurantListWrapper} />
+    <Route  path="restaurants/:name" component={RestaurantDetails} />
+
+  </Route>
+);
