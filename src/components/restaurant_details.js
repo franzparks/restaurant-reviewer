6cb01bf2f7f988be31268b70940213cbdc8a884a
franzparks <franzparks@gmail.com>
2016-06-29 19:41:52 -0700

    add some details

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -3,7 +3,15 @@ import React from 'react';
 const RestaurantDetails = (props) =>{
 
 	return (
-		<div>A lot of  Details to come! </div>
+		<div className="col-xs-6 col-lg-4">
+	        <h2>{props.name}</h2>
+	        <p>{props.image}</p>
+	        <p>Address: {props.address}</p>
+	        <p>Type: {props.cuisineType}</p>
+	        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        <Link to="details"> Details</Link>
+
+	    </div>
 	);
 
 }
