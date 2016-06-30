16911216c3846b26913804fc5f29d8cdd2784647
franzparks <franzparks@gmail.com>
2016-06-29 22:12:40 -0700

    use props

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const RestaurantDetails = (props) =>{
-	console.log("data : "+Object.keys(props.params));
-	console.log("data : "+props.params.name);
+	console.log("data : "+Object.keys(props.restaurants));
+	console.log("data : "+props.restaurants[0]);
 	//var name = props.params.name;
 	var restaurant = props.restaurants.find((res) =>{
-		return res.name === props.params.name;
+		console.log("name "+Object.keys(res.props));
+		return res.props.name === props.params.name;
 	});
 	console.log("restaurant "+restaurant);
 
 	return (
 		<div className="col-xs-6 col-lg-4">
-	        <h2>{restaurant.name}</h2>
-	        <p>{restaurant.image}</p>
-	        <p>Address: {restaurant.address}</p>
-	        <p>Type: {restaurant.cuisineType}</p>
+	        <h2>{restaurant.props.name}</h2>
+	        <p>{restaurant.props.image}</p>
+	        <p>Address: {restaurant.props.address}</p>
+	        <p>Type: {restaurant.props.cuisineType}</p>
 	        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 	        <Link to="/"> Back</Link>
 
