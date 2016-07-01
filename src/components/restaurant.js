a9928fa052b7b86c45f8d186f35c5b4cd7c870f4
franzparks <franzparks@gmail.com>
2016-06-30 22:24:39 -0700

    update link

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -7,12 +7,12 @@ const Restaurant = (props) => {
 	
 	return(
 	    <div className="col-xs-6 col-lg-4">
+	        
 	        <h2>{props.name}</h2>
 	        <p>{props.image}</p>
 	        <p>Address: {props.address}</p>
 	        <p>Type: {props.cuisineType}</p>
-	        <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-	        <Link to={path} > Details</Link>
+	        <Link to={path} className="btn btn-default" > View details &raquo;</Link>
 
 	    </div>
     );
