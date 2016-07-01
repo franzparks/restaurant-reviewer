2d33d03e29cda46de12334ec6fddf21a5514aa38
franzparks <franzparks@gmail.com>
2016-07-01 15:35:03 -0700

    fix call

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const RestaurantDetails = (props) =>{
-	console.log("data : "+Object.keys(props));
+	//console.log("data : "+Object.keys(props));
 
-	console.log("single restaurant from app state : "+props.fetchRestaurant(props.params.id));
+	//console.log("single restaurant from app state : "+props.fetchRestaurant(props.params.id));
 
-	var restaurant1 = props.restaurants.find((res) =>{
+	//var restaurant1 = props.restaurants.find((res) =>{
 		
-		return res.props.id === props.params.id;
-	});
+	//	return res.props.id === props.params.id;
+	//});
 
 	var restaurant = props.fetchRestaurant(props.params.id);
+	console.log("keys on restaurant  : "+restaurant);
 
 	//to do: display reviews from rawData
 
@@ -21,10 +22,10 @@ const RestaurantDetails = (props) =>{
 				<button>Write A Review </button>
 			</div>
 			<div className="col-xs-6 col-lg-4">
-		        <h2>{restaurant.props.name}</h2>
-		        <p>{restaurant.props.image}</p>
-		        <p>Address: {restaurant.props.address}</p>
-		        <p>Type: {restaurant.props.cuisineType}</p>
+		        <h2>{restaurant.name}</h2>
+		        <p>{restaurant.image}</p>
+		        <p>Address: {restaurant.address}</p>
+		        <p>Type: {restaurant.cuisineType}</p>
 		        
 		        <Link to="/"> Back</Link>
 
