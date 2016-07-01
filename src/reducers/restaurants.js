cfda40abc38598a1c19393a10090ba60cdfea929
franzparks <franzparks@gmail.com>
2016-07-01 14:21:47 -0700

    use initial state

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -3,11 +3,13 @@ import {
 }from '../actions/types';
 
 let INITIAL_STATE = { restaurants = [] ,restaurant = null };
-export default function(state = [], action){
+export default function(INITIAL_STATE, action){
 	switch(action.type) {
 		case FETCH_RESTAURANTS:
 		    console.log(action.payload);
-		    return [ ...state, ...action.payload.data ];
+		    //return [ ...state, ...action.payload.data ];
+		    return { ...INITIAL_STATE, ...INITIAL_STATE.restaurants,...action.payload.data};
+		    
 		case FETCH_RESTAURANT:
 			return {};    
 	}
