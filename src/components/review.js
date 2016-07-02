b81afdcb6c112480beae146751d075fd67ad1935
franzparks <franzparks@gmail.com>
2016-07-01 22:31:10 -0700

    add onsubmit handler

--- src/components/review.js
+++ src/components/review.js
@@ -9,6 +9,16 @@ class Review extends Component {
     	router: PropTypes.object
     };
 
+    onSubmit(props) {
+    this.props.postReview(props)
+	    .then(() => {
+	        // review  has been created, navigate the user to the index
+	        // We navigate by calling this.context.router.push with the
+	        // new path to navigate to.
+	        this.context.router.push('/');
+	    });
+    }
+
 	render(){
 	return (
     <div> Write a review </div>
