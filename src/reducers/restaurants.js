84b67fbc876983fb25461c36dd9974024f8262cf
franzparks <franzparks@gmail.com>
2016-07-01 18:07:16 -0700

    clean up

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -7,15 +7,14 @@ const INITIAL_STATE = { restaurants : [] ,restaurant : null };
 
 export default function(state = INITIAL_STATE, action){
 	switch(action.type) {
+
 		case FETCH_RESTAURANTS:
-		    //console.log(action.payload);
 		    return { ...state, restaurants: action.payload.data };
 
+
 		case FETCH_RESTAURANT:
-		    //console.log("action :"+action.restaurant_id);
 			return { ...state, 
 				    restaurant : state.restaurants.find( (res) =>{
-					//console.log(" id : "+restaurant.id);
 					return res.id === action.restaurant_id;
 				}) 
 			};
