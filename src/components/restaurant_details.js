1eff0a3309e7b763443bc602ea57c6fd998acc12
franzparks <franzparks@gmail.com>
2016-07-01 17:00:16 -0700

    add lifecycle method

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router';
 class RestaurantDetails  extends Component{
 	//console.log("data : "+Object.keys(props));
 
+	componentWillMount(){
+		props.fetchRestaurant(props.params.id);
+	}
+
 	//console.log("single restaurant from app state : "+props.fetchRestaurant(props.params.id));
 
 	//var restaurant1 = props.restaurants.find((res) =>{
 		
 	//	return res.props.id === props.params.id;
 	//});
-   props.fetchRestaurant(props.params.id);
+   
 	//var restaurant = props.fetchRestaurant(props.params.id);
 	//console.log("keys on restaurant  : "+Object.keys(props.restaurant));
 
