5046656482d4895649727b3df8fbe217a318aec9
franzparks <franzparks@gmail.com>
2016-07-01 22:45:31 -0700

    import redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import restaurantsReducer from './restaurants';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   appState: restaurantsReducer
