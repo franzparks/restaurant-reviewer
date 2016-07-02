f0135066701a885dc141fe6b990f8b3ce016e800
franzparks <franzparks@gmail.com>
2016-07-01 22:46:09 -0700

    add form to app state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,8 @@ import restaurantsReducer from './restaurants';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  appState: restaurantsReducer
+  appState: restaurantsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
