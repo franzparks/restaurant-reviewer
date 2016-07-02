e14a55b6bd7fb73fa0adc0e8b471e87ef46b376e
franzparks <franzparks@gmail.com>
2016-07-01 17:11:51 -0700

    import actions and connect

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -1,4 +1,7 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 import { Link } from 'react-router';
 
 class RestaurantDetails  extends Component{
