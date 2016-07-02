8233e768379e8e67ce6a5c1aac8f5b9d5b471922
franzparks <franzparks@gmail.com>
2016-07-01 22:26:45 -0700

    import post review

--- src/components/review.js
+++ src/components/review.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
+import { postReview } from '../actions/index';
 
 const Review = (props) => {
 	return (
