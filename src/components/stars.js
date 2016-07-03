66286b7d67205c987f143ba5162de70ffe563c68
franzparks <franzparks@gmail.com>
2016-07-03 12:46:54 -0700

    update font size

--- src/components/stars.js
+++ src/components/stars.js
@@ -7,6 +7,7 @@ return (
 
 	<div className="rating">
       <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+      <input type="radio" id="star5" name="rating" value="5" /><label htmlFor="star5" title="Rocks!">5 stars</label>
      </div>
 );
 
