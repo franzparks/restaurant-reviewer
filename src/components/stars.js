172ebaeb5ee00d738cd0e4eb2f39f5b63cdeb3a5
franzparks <franzparks@gmail.com>
2016-07-02 21:28:03 -0700

    change stars

--- src/components/stars.js
+++ src/components/stars.js
@@ -1,19 +1,15 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const Stars = (props) => {
 
 return (
 	
 	<div>
-	<h1 style="margin: 0">Hover Me</h1>
+	<div className="rating">
+<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+</div>
 
-	<span className="rating">
-  <span className="star" title="5 - I love it"></span>
-  <span className="star" title="4 - I really like it"></span>
-  <span className="star" title="3 - I like it"></span>
-  <span className="star" title="2 - It&#39;s ok"></span>
-  <span className="star" title="1 - Not for me"></span>
-</span>
 	</div>
 	
 );
