dbfb3537d996d54e76a73f5121f16e752854d69d
franzparks <franzparks@gmail.com>
2016-07-03 12:48:06 -0700

    make stars as inputs

--- src/components/stars.js
+++ src/components/stars.js
@@ -6,8 +6,11 @@ const Stars = (props) => {
 return (
 
 	<div className="rating">
-      <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-      <input type="radio" id="star5" name="rating" value="5" /><label htmlFor="star5" title="Rocks!">5 stars</label>
+		<input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
+		<input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty Good!">4 stars</label>
+		<input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh!">3 stars</label>
+		<input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda Bad">2 stars</label>
+		<input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label>
      </div>
 );
 
