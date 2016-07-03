084befc9d5128da32d8a7c3d9856b677e39b0d37
franzparks <franzparks@gmail.com>
2016-07-03 00:11:42 -0700

    add font size

--- src/components/stars.js
+++ src/components/stars.js
@@ -4,16 +4,10 @@ import FontAwesome from 'react-fontawesome';
 const Stars = (props) => {
 
 return (
-	
-	<div>
+
 	<div className="rating">
-<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-</div>
-<br/>
-
-	</div>
-
-	
+      <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+     </div>
 );
 
 }
