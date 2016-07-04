55b5ecd8281aa6bf42544cf442ef9c94d3ce6a38
franzparks <franzparks@gmail.com>
2016-07-04 10:23:34 -0700

    remove import

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
-import RestaurantContainer from './restaurant_container';
-
 import Restaurant from './restaurant';
 import Footer from './footer';
 import Menu from './menu';
