b63a199238e12f73b9318aa6ad1779311c560d49
franzparks <franzparks@gmail.com>
2016-07-04 10:47:27 -0700

    map state to one restaurant

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -84,6 +84,6 @@ function mapStateToProps(state) {
 	//console.log("app state : "+Object.keys(state.appState));
 	//console.log("restaurants : "+ state.appState.restaurants)
 	//console.log("restaurant : "+ state.appState.restaurant)
-	return { restaurants : state.appState.restaurants };
+	return { restaurant : state.appState.restaurant };
 }
 export default connect(mapStateToProps,actions)(RestaurantDetails);
