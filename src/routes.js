63acc77454ac44e2e8fedaec9e9fbd4ee499566a
franzparks <franzparks@gmail.com>
2016-07-03 23:37:29 -0700

    rename compononent

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,14 @@ import App from './components/app';
 import MainLayout from './components/main_layout';
 import RestaurantList from './components/restaurant_list';
 import RestaurantDetails from './components/restaurant_details';
-import Review from './components/review';
+import CreateReview from './components/create_review';
 
 
 export default (
   <Route path="/" component={MainLayout}>
     <IndexRoute component={RestaurantList} />
     <Route  path="restaurants/:id" component={RestaurantDetails} />
-    <Route  path="restaurants/:id/reviews/new" component={Review}/>
+    <Route  path="restaurants/:id/reviews/new" component={CreateReview}/>
 
   </Route>
 
