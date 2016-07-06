ec0d54b37a6a6ff6d4209a5b92b0c7d5b3224731
franzparks <franzparks@gmail.com>
2016-07-05 23:08:50 -0700

    clean up

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -9,26 +9,17 @@ const INITIAL_STATE = { restaurants : [] ,restaurant : null };
 export default function(state = INITIAL_STATE, action){
 	switch(action.type) {
 
-        /* Will discontinue using until attached to actual api*/
 		case FETCH_RESTAURANT:
-			//console.log("action is passing id : "+ action.restaurant_id);
-			//var restaurant1 = state.restaurants.find( (res) =>{
-			//		return res.id === action.restaurant_id;
-			//});
-			//state.restaurant = restaurant;
-			//console.log(" found : "+restaurant);
-			return { ...state,
-				    restaurant : action.restaurant
-			};
+		
+			return { ...state,  restaurant : action.restaurant };
 
 		case FETCH_RESTAURANTS:
 
 		    return { ...state, restaurants: action.payload.data };
 
         case POST_REVIEW:
-            console.log(" action keys : "+ Object.keys(action));
-            //console.log("got id : "+action.review.id + " ..and date : "+action.review.date +" ..and rating : "+ action.review.rating);
-        	return { ...state , restaurants :[ ...state.restaurants, action.restaurant] }; //, ...state.restaurants, {action.restaurant}
+           
+        	return { ...state , restaurants :[ ...state.restaurants, action.restaurant] };
 	
 
 		default: 
