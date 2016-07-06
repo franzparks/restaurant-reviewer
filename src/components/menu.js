37628cf8ddf36aa645b60937b87a7f65f298b317
franzparks <franzparks@gmail.com>
2016-07-05 22:28:54 -0700

    import actions

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
 import MenuItem from './menuitem';
 
 class Menu extends Component {
