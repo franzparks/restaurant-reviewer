9e993df8821be6ca9ce9a8aeb959eb0ad4111174
franzparks <franzparks@gmail.com>
2016-07-05 23:16:16 -0700

    clean up

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -15,37 +15,21 @@ class MainLayout extends Component {
         super(props);
     }
 
-
-	
-    
 	render() {
-		//var results = this.loadData(this.props.restaurants);
-        //var results = this.props.restaurants;
-		//results = this.filterRestaurants(results, this.state.filterText, this.state.filterCategory);
 		
         return (
             <div>
-                <Header 
-                	//handleChange={this.handleChange}
-                />
-
+                <Header />
 		  	    <div className="container">
 		            <div className="row row-offcanvas row-offcanvas-right">
 		                <div className="col-xs-12 col-sm-9">
 					        <ToggleNav />                  
-                            <div>{React.cloneElement(this.props.children,
-	                            { 
-		                            //restaurants: this.props.restaurants,
-                                   // filterText: this.state.filterText,
-                                    //filterCategory: this.state.filterCategory
-	                            } 
-                             )}
+                            <div>
+                            {this.props.children}
                             </div> 
                            
 					    </div>
-					    <Menu 
-					    	//selectionHandler={this.handleSelection}
-					    />
+					    <Menu />
 					         	        
 		            </div>
 		            <Footer />
