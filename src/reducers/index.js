5b6f3c93ee53523cd6fb3afb05ff0caa5e5ef687
franzparks <franzparks@gmail.com>
2016-07-05 22:12:54 -0700

    add filters reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import restaurantsReducer from './restaurants';
+import filtersReducer from './filters';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   appState: restaurantsReducer,
-  form: formReducer
+  form: formReducer,
+  filtersReducer: filtersReducer
+
 });
 
 export default rootReducer;
