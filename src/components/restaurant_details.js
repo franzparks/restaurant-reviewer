068e89fca514d6759e015ba3f5cb6af6bf296d12
franzparks <franzparks@gmail.com>
2016-07-09 15:49:40 -0700

    import stars component

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 import { Link } from 'react-router';
+import Stars from './stars';
 
 import Review from './review';
 
