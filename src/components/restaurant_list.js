d8aceab7fc9b04a2bb7c095a19d7a5230682085b
franzparks <franzparks@gmail.com>
2016-07-11 15:26:20 -0700

    add comment for later

--- src/components/restaurant_list.js
+++ src/components/restaurant_list.js
@@ -124,7 +124,7 @@ class RestaurantList extends Component {
                 address={restaurant.address}
                 cuisineType={restaurant.cuisineType}
                 hours={restaurant.operatingHours}
-                averageRating: {restaurant.averageRating}
+                averageRating: {restaurant.averageRating} //this will be average rating
                 numberOfRatings : {restaurant.numberOfRatings}
                 checked_star={'5'}
                
