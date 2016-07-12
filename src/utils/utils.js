82034a84e583a7d9285897fac3ca0579d73ac9fa
franzparks <franzparks@gmail.com>
2016-07-12 10:33:11 -0700

    export more util functions to util component

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,3 +11,56 @@ export  const getAverageRating = (reviews) => {
     }else
         return 0; 
 }
+
+
+export const sortUsingCategoryFilter = (arr,filterCategory) => {
+	var sorted = [];
+	if(filterCategory){
+        
+        if(filterCategory === 'all'){
+            /* Do Nothing */
+            return arr;
+        }else{
+                sorted = arr.sort((a,b) => {
+
+                if(filterCategory === 'highestRated' && arr.length > 2){
+
+                	return a.averageRating < b.averageRating;
+
+                }else if(filterCategory === 'mostReviewed' && arr.length > 2){
+                    return a.numberOfRatings < b.numberOfRatings;
+                }
+            });
+
+            return sorted;
+        }
+    }else
+    	return arr;
+}
+
+
+export const  sortUsingTextFilter = (arr,text) => {
+   /*ToDo : filter better by ignoring case and using contains or something similar*/
+  
+	return arr.filter((R) =>{
+
+      if(R.name.indexOf(text) !== -1 || R.address.indexOf(text) !== -1  || R.cuisineType.indexOf(text) !== -1){
+        return R;
+      }
+      
+	});
+	
+}
+
+export const  filterRestaurants = (arr, text, category) => {
+    
+	if(text){
+		return this.sortUsingTextFilter(arr,text);
+	}
+
+	if(category){
+        return this.sortUsingCategoryFilter(arr,category);
+	}
+
+	return arr;
+}
