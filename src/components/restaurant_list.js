1744157f5658cb0a4ee37c2ccdb13f2eab53decd
franzparks <franzparks@gmail.com>
2016-07-12 10:57:58 -0700

    remove unused functions

--- src/components/restaurant_list.js
+++ src/components/restaurant_list.js
@@ -22,12 +22,6 @@ class RestaurantList extends Component {
 
     	this.renderList = this.renderList.bind(this);
     	this.handleClick = this.handleClick.bind(this);
-
-        this.getAverageRating = this.getAverageRating.bind(this);
-        this.getNumberOfRatings = this.getNumberOfRatings.bind(this);
-        this.sortUsingCategoryFilter = this.sortUsingCategoryFilter.bind(this);
-        this.filterRestaurants = this.filterRestaurants.bind(this);
-        this.sortUsingTextFilter = this.sortUsingTextFilter.bind(this);
     }
 
     componentWillMount() {
@@ -37,79 +31,6 @@ class RestaurantList extends Component {
     handleClick(restaurant){
         this.props.fetchRestaurant(restaurant);
     }
-
-    sortUsingCategoryFilter = (arr,filterCategory) => {
-    	var sorted = [];
-    	if(filterCategory){
-            
-            if(filterCategory === 'all'){
-                /* Do Nothing */
-                return arr;
-            }else{
-                    sorted = arr.sort((a,b) => {
-
-	                if(filterCategory === 'highestRated' && arr.length > 2){
-
-	                	return a.averageRating < b.averageRating;
-
-	                }else if(filterCategory === 'mostReviewed' && arr.length > 2){
-	                    return a.numberOfRatings < b.numberOfRatings;
-	                }
-                });
-
-                return sorted;
-            }
-        }else
-        	return arr;
-    }
-
-    sortUsingTextFilter = (arr,text) => {
-       /*ToDo : filter better by ignoring case and using contains or something similar*/
-      
-    	return arr.filter((R) =>{
-
-          if(R.name.indexOf(text) !== -1 || R.address.indexOf(text) !== -1  || R.cuisineType.indexOf(text) !== -1){
-            return R;
-          }
-          
-    	});
-    	
-    }
-
-    filterRestaurants = (arr, text, category) => {
-    
-    	if(text){
-    		return this.sortUsingTextFilter(arr,text);
-    	}
-
-    	if(category){
-            return this.sortUsingCategoryFilter(arr,category);
-    	}
-
-    	return arr;
-    }
-
-    getAverageRating = (reviews) => {
-       
-        if(reviews  && reviews.length > 1){
-            
-            var sum = 0;
-            reviews.forEach((review) =>{
-                sum += review.rating;
-            });
-
-            return Math.floor(sum / reviews.length);
-        }else
-            return 0; 
-    }
-
-    getNumberOfRatings = (reviews) => {
-        if(reviews  && reviews.length > 1){
-            return reviews.length;
-        }
-        return 0; 
-    }
-
 
     renderList(restaurants) {
     	var filteredRestaurants = [];
