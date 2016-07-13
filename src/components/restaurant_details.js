c2a9c48f21e690ea3fa95cecf235b0fb5692d34b
franzparks <franzparks@gmail.com>
2016-07-13 15:17:39 -0700

    add review button

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -26,57 +26,63 @@ class RestaurantDetails  extends Component{
 
             <div className="row">
 					
-            <div className="col-xs-6 col-lg-6 ">
+                <div className="col-xs-6 col-lg-6 ">
 
-				<div className="row">
-                    <h2>{this.props.restaurant.name}</h2>
-		            <p>
-                    <img src={this.props.restaurant.image} 
-                    className="img-responsive" 
-                    alt="Image of restaurant cuisine type">
-                    </img>
-                    </p>
+    				<div className="row">
+                        <h2>{this.props.restaurant.name}</h2>
+    		            <p>
+                        <img src={this.props.restaurant.image} 
+                        className="img-responsive" 
+                        alt="Image of restaurant cuisine type">
+                        </img>
+                        </p>
 
-                    <div aria-label="Average Rating" className="flex-container">
-                        <Stars 
-                            key={Math.random() * 10000}
-                            style={'rating'}
-                            condition={false}
-                            checked_star={getAverageRating(this.props.restaurant.reviews)} 
-                            type={'checkbox'}
-                            keys={[
-                                this.props.key ,this.props.key,this.props.key,
-                                this.props.key,this.props.key
-                            ]}
-                        />
-                        <div className="flex-item">
-                            {getNumberOfRatings(this.props.restaurant.reviews)} Reviews
+                        <div aria-label="Average Rating" className="flex-container">
+                            <Stars 
+                                key={Math.random() * 10000}
+                                style={'rating'}
+                                condition={false}
+                                checked_star={getAverageRating(this.props.restaurant.reviews)} 
+                                type={'checkbox'}
+                                keys={[
+                                    this.props.key ,this.props.key,this.props.key,
+                                    this.props.key,this.props.key
+                                ]}
+                            />
+                            <div className="flex-item">
+                                {getNumberOfRatings(this.props.restaurant.reviews)} Reviews
+                            </div>
                         </div>
+
                     </div>
+                        
+                    <div className="row">
+
+                        <div aria-label="Cuisine Type" className="flex-item">
+                            <strong>Cuisine Type:</strong>  {this.props.restaurant.cuisineType}
+                        </div>
+
+                        <div aria-label="Address" className="flex-item">
+                            <strong>Address:</strong>  {this.props.restaurant.address}
+                        </div>
+
+                        <div aria-label="Hours Open" className="flex-item">
+                            <strong>Hours Open:</strong>  {this.props.restaurant.operatingHours.open} AM to        
+                            {this.props.restaurant.operatingHours.close } PM
+                        </div>
+
+    			        <Link to="/" className="btn btn-primary flex-item"> Back</Link>
+
+    			    </div>
+
+
+    			</div>
+
+                 <div className="col-sm-6 col-lg-6">
+
+                    <Link to={path} className="btn btn-primary" >Write A Review </Link>
 
                 </div>
-                    
-                <div className="row">
-
-                    <div aria-label="Cuisine Type" className="flex-item">
-                        <strong>Cuisine Type:</strong>  {this.props.restaurant.cuisineType}
-                    </div>
-
-                    <div aria-label="Address" className="flex-item">
-                        <strong>Address:</strong>  {this.props.restaurant.address}
-                    </div>
-
-                    <div aria-label="Hours Open" className="flex-item">
-                        <strong>Hours Open:</strong>  {this.props.restaurant.operatingHours.open} AM to        
-                        {this.props.restaurant.operatingHours.close } PM
-                    </div>
-
-			        <Link to="/" className="btn btn-primary flex-item"> Back</Link>
-
-			    </div>
-
-
-			</div>
 
             </div>
 		  
