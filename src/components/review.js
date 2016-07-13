8eac2169e0babb2f87297138dc9e1448b12ce03e
franzparks <franzparks@gmail.com>
2016-07-12 22:30:10 -0700

    add stars to review component

--- src/components/review.js
+++ src/components/review.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Stars from './stars';
 
 const Review = (props) => {
 
@@ -7,7 +8,26 @@ const Review = (props) => {
 	    <div className="col-xs-8">
 	        <hr />
 	        <h4>{props.name}</h4>
-	        <p><strong>Rating : </strong> {props.rating}</p>
+	        <div className="flex-container">
+	        <strong>Rating : </strong> 
+	       
+	        <div aria-label="Reviewer's Rating" >
+                        <Stars 
+                            key={Math.random() * 10000}
+                            style={'rating'}
+                            condition={false}
+                            checked_star={props.rating} 
+                            type={'checkbox'}
+                            keys={[
+            					Math.random() * 100000,
+            					Math.random() * 100000,
+            					Math.random() * 100000,
+            					Math.random() * 100000,Math.random() * 100000
+            				]}
+                        />
+                       
+                    </div>
+	        </div>
 	        <p><strong>Comment: </strong> {props.comment}</p>
 	        <p><strong>Date: </strong> {props.date}</p>
 	        
