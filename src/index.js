14ae1fd5efd768a61cb8d2ba371bf92ba1a2858c
franzparks <franzparks@gmail.com>
2016-07-15 15:47:46 -0700

    remove jquery import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
-import * as $ from '../assets/jquery.min';
 
 import routes from './routes';
 
