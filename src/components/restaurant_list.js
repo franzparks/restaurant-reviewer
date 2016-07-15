0f76d80535e568bac771ca11457345c2eee4729b
franzparks <franzparks@gmail.com>
2016-07-14 23:01:58 -0700

    add keys

--- src/components/restaurant_list.js
+++ src/components/restaurant_list.js
@@ -83,14 +83,14 @@ class RestaurantList extends Component {
                 <div className="row row-offcanvas row-offcanvas-right">
                     <div className="col-xs-12 col-sm-6">
                         <ToggleNav key={Math.random()}/>
-                        <Overview />                
-                        <div>
+                        <Overview key={Math.random()}/>                
+                        <div key={Math.random()}>
                         {this.renderList(restaurants)} 
 
                         </div> 
                        
                     </div>
-                    <Menu /> 
+                    <Menu key={Math.random()} /> 
                                     
                 </div>
 		    	
