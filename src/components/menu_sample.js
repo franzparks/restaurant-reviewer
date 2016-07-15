10b2f838e81b2585d86f84395ee2dc261f89c41f
franzparks <franzparks@gmail.com>
2016-07-14 20:18:05 -0700

    add name and on click handler

--- src/components/menu_sample.js
+++ src/components/menu_sample.js
@@ -15,7 +15,7 @@ class SampleMenu extends Component{
         super(props);
         this.state = {"selected" :"list-group-item active", "unselected":"list-group-item", "style":"", "selectedItem":""};
         this.handleClick = this.handleClick.bind(this);
-        this.displayMenuItems = this.displayMenuItems.bind(this);
+        //this.displayMenuItems = this.displayMenuItems.bind(this);
         
     }
     
@@ -25,10 +25,11 @@ class SampleMenu extends Component{
 
     handleClick (e){
 
-        this.props.setCategoryFilter(e.target.name);
+        //this.props.setCategoryFilter(e.target.name);
 
         /* Identify selected item*/
-        this.setState({selectedItem : e.target.name});
+        //this.setState({selectedItem : e.target.name});
+        console.log(e.target.name);
   }
 
 
@@ -37,11 +38,11 @@ class SampleMenu extends Component{
      return (
         <ButtonToolbar>
         <DropdownButton bsStyle={'primary'} title={'Toggle Nav'} key={Math.random() * 1000} id={'dropdown-basic-0'}>
-          <MenuItem eventKey="1">All</MenuItem>
+          <MenuItem eventKey="1" name="all" onClick={this.handleClick}>All</MenuItem>
           <MenuItem divider />
-          <MenuItem eventKey="2">Highest Rated</MenuItem>
-          <MenuItem eventKey="3" active>Most Reviewd</MenuItem>
-          <MenuItem eventKey="4">Open Now</MenuItem>
+          <MenuItem eventKey="2" name="highestRated" onClick={this.handleClick}>Highest Rated</MenuItem>
+          <MenuItem eventKey="3" name="mostReviewed" onClick={this.handleClick}>Most Reviewd</MenuItem>
+          <MenuItem eventKey="4" name="openNow" onClick={this.handleClick}>Open Now</MenuItem>
         </DropdownButton>
         </ButtonToolbar>
      );
