81a495d6e91f049d82ee7644cf777feaf4a438aa
franzparks <franzparks@gmail.com>
2016-07-14 20:04:41 -0700

    turn sample menu into class component

---
+++ src/components/menu_sample.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import ButtonGroup, {DropdownButton,MenuItem, ButtonToolbar} from 'react-bootstrap';
+
+
+class SampleMenu extends Component{
+
+
+  render(){
+
+     return (
+        <ButtonToolbar>
+        <DropdownButton bsStyle={'primary'} title={'Toggle Nav'} key={Math.random() * 1000} id={'dropdown-basic-0'}>
+          <MenuItem eventKey="1">All</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey="2">Highest Rated</MenuItem>
+          <MenuItem eventKey="3" active>Most Reviewd</MenuItem>
+          <MenuItem eventKey="4">Open Now</MenuItem>
+        </DropdownButton>
+        </ButtonToolbar>
+     );
+ }
+
+} 
+
+
+export default connect(null, actions)(SampleMenu);
