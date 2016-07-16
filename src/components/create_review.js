5190fbb5090d014892f35c5ce3085ad55f74a19a
franzparks <franzparks@gmail.com>
2016-07-16 15:31:59 -0700

    update styling of form

--- src/components/create_review.js
+++ src/components/create_review.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { postReview } from '../actions/index';
-//import { Link } from 'react-router';
+import { Link } from 'react-router';
 import Stars from './stars';
 
 import {getCurrentDate} from '../utils/utils';
@@ -15,98 +15,141 @@ class ReviewForm extends Component {
     };
 
     constructor(props){
-      super(props);
+        super(props);
 
         this.onSubmit = this.onSubmit.bind(this);
-        this.getRating = this.getRating.bind(this);
+        //this.getRating = this.getRating.bind(this);
         //this.renderForm = this.renderForm.bind(this);
     }
 
     onSubmit(props) {
 
-      //console.log("props : "+ props);
-      //console.log("restaurant : "+ Object.keys(this.props.restaurant[0]));
-      var path = `/restaurants/${this.props.params.id}`;
-      
-      const review = {
-        id : Math.random() * 10000 ,
-        ...props, 
-        date: getCurrentDate()
-      };
+        //console.log("props : "+ props);
+        //console.log("restaurant : "+ Object.keys(this.props.restaurant[0]));
+        var path = `/restaurants/${this.props.params.id}`;
+          
+        const review = {
+            id : Math.random() * 10000 ,
+            ...props, 
+            date: getCurrentDate()
+        };
 
-      var restaurant = this.props.restaurant[0];
-        restaurant.reviews = [ ...restaurant.reviews, review];
- 
+        var restaurant = this.props.restaurant[0];
+            restaurant.reviews = [ ...restaurant.reviews, review];
+
         this.props.postReview(restaurant);  
 
-      this.context.router.push(path);
+        this.context.router.push(path);
     }
 
 
   render() {
 
-
     const {
-      fields: { name, rating, comment },
-      handleSubmit,
-      resetForm,
-      submitting
-      } = this.props
+        fields: { name, rating, comment },
+        handleSubmit,
+        resetForm,
+        submitting
+    } = this.props
 
 
     return (
-
-      <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="form-group">
-        <div>
-          <label>Name</label>
-          <div>
-            <input type="text" placeholder="Name" {...name}/>
-          </div>
-        </div>
+        <div className="col-xs-12 col-lg-6">
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} >
+            
+            <div className="form-group">
+                <label htmlFor="name" className="control-label">Name</label>
+                <div>
+                    <input type="text" placeholder="Name" {...name} id="name" className="form-control"/>
+                </div>
+            </div>
         
-        <div>
-          <label>Rate</label>
-          <div>
-            <label>
-              <input type="radio" {...rating} value="5" checked={rating.value === '5'} className="rating"/> 5 stars
-            </label>
-            <label>
-              <input type="radio" {...rating} value="4" checked={rating.value === '4'} className="rating"/> 4 stars
-            </label>
-            <label>
-              <input type="radio" {...rating} value="3" checked={rating.value === '3'} className="rating"/> 3 stars
-            </label>
-            <label>
-              <input type="radio" {...rating} value="2" checked={rating.value === '2'} className="rating"/> 2 stars
-            </label>
-            <label>
-              <input type="radio" {...rating} value="1" checked={rating.value === '1'} className="rating"/> 1 stars
-            </label>
-          </div>
-        </div>  
+            <div className="form-group">
+                <label >Rate Restaurant</label>
+
+                <div className="rating">
+                    
+                    <label htmlFor="5">
+                        <input type="radio" {...rating} 
+                            value="5" 
+                            id="5"
+                            checked={rating.value === '5'} 
+                            name="rating" 
+                            title="Rocks!"
+                            /> 5 stars
+                    </label>
+            
+                    <label htmlFor="4">
+                        <input type="radio" {...rating} 
+                            value="4" 
+                            id="4"
+                            checked={rating.value === '4'} 
+                            name="rating" 
+                            title="Pretty Good!"
+                        /> 4 stars
+                    </label>
+
+                    <label htmlFor="3">
+                        <input type="radio" {...rating} 
+                            value="3"
+                            id="3" 
+                            checked={rating.value === '3'}
+                            name="rating" 
+                            title="Meh!"
+                        /> 3 stars
+                    </label>
+
+                    <label htmlFor="2">
+                        <input type="radio" {...rating}
+                            value="2"
+                            id="2" 
+                            checked={rating.value === '2'} 
+                            name="rating" 
+                            title="Kinda Bad"
+                        /> 2 stars
+                    </label>
+
+                    <label htmlFor="1">
+                        <input type="radio" {...rating}
+                            value="1"
+                            id="1" 
+                            checked={rating.value === '1'} 
+                            name="rating" 
+                            title="Sucks big time"
+                        /> 1 stars
+                    </label>
+
+                </div>
+            </div>  
        
-        <div>
-          <label>Comment</label>
-          <div>
-            <textarea
-              {...comment}
-              // required for reset form to work (only on textarea's)
-              // see: https://github.com/facebook/react/issues/2533
-              value={comment.value || ''}/>
-          </div>
-        </div>
-        <div>
-          <button type="submit" disabled={submitting}>
-            {submitting ? <i/> : <i/>} Submit
-          </button>
-          <button type="button" disabled={submitting} onClick={resetForm}>
-            Clear Values
-          </button>
-          <button type="button" disabled={submitting} onClick={resetForm}>
-            Cancel
-          </button>
-        </div>
-      </form>
+            <div className="form-group">
+                <label htmlFor="comment" className="control-label">Comment</label>
+                <div>
+                    <textarea
+                        {...comment}
+                        id="comment"
+                        // required for reset form to work (only on textarea's)
+                        // see: https://github.com/facebook/react/issues/2533
+                        value={comment.value || ''}
+                        className="form-control"
+                    />
+                </div>
+            </div>
+
+            <div className="form-group">
+                <button type="submit" disabled={submitting} className="btn btn-primary btn-lg">
+                    {submitting ? <i className="fa fa-paper-plane" /> : <i className="fa fa-paper-plane"/>} Submit
+                </button>
+                <button type="button" disabled={submitting} onClick={resetForm} className="btn btn-default btn-lg">
+                    Clear Values
+                </button>
+                <button type="button" disabled={submitting} onClick={resetForm} className="btn btn-default btn-lg">
+                    Cancel
+                </button>
+            </div>
+
+        </form>
+    </div>
     )
   }
 }
