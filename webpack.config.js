5f9f72b51a19324c5c6eb9d6d3c46a44effd1469
franzparks <franzparks@gmail.com>
2016-07-17 14:30:12 -0700

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+      
+    },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
+
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
