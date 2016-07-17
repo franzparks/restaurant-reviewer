d0f84f3fae14664b1a7ca07b9e7e8662ab4bf826
franzparks <franzparks@gmail.com>
2016-07-17 15:57:11 -0700

    remove unused file

--- webpack.config.js
+++ webpack.config.js
@@ -9,21 +9,34 @@ module.exports = {
     
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
+    loaders: [
+        {
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
       
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-      
-    }
+        },
+        {   
+            test: /\.css$/, 
+            loader: "css-loader!autoprefixer-loader"
+        },
+        {   
+            test: /\.png$/,
+            loader: "url-loader?limit=100000"
+        },
+
+        {   
+            test: /\.jpg$/,
+            loader: "file-loader"
+        } 
   
 
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.css', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
