ec582bcd47ab732018fc2565eb4365f5fe5d1830
franzparks <franzparks@gmail.com>
2016-07-18 16:43:51 -0700

    remove styling class

--- src/components/restaurant_details.js
+++ src/components/restaurant_details.js
@@ -41,7 +41,7 @@ class RestaurantDetails  extends Component{
                         <h4>{restaurant.name}</h4>
                         <p>
                         <img src={restaurant.image} 
-                        className="img-responsive img-focused" 
+                        className="img-responsive" 
                         alt="Image of restaurant cuisine type">
                         </img>
                         </p>
