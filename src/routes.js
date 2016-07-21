d481273852678c608dd2836f17b3cc3059b009f0
franzparks <franzparks@gmail.com>
2016-07-20 21:07:31 -0700

    add page not found component

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,15 @@ import RestaurantList from './components/restaurant_list';
 import RestaurantDetails from './components/restaurant_details';
 import CreateReview from './components/create_review';
 
+const NotFound = React.createClass({
+  render() {
+    return <h2>Not found</h2>
+  }
+});
 
 export default (
+
+	<div>
   <Route path="/" component={MainLayout}>
     <IndexRoute component={RestaurantList} />
     <Route  path="restaurants/:id" component={RestaurantDetails} />
@@ -17,6 +24,21 @@ export default (
  	<Redirect from="restaurants/:id/reviews" to=":id/reviews" />
 
   </Route>
+
+  <Route path="/#/" component={MainLayout}>
+    <IndexRoute component={RestaurantList} />
+    <Route  path="/#/restaurants/:id" component={RestaurantDetails} />
+    <Redirect from="restaurants/:id/reviews" to="/#/restaurants/:id" />
+
+    <Route  path="/#/:id/reviews" component={CreateReview} />
+
+ 	<Redirect from="/#/restaurants/:id/reviews" to="/#/:id/reviews" />
+
+ 	<Route path="*" component={NotFound} />
+
+  </Route>
+
+  </div>
 
 
 
